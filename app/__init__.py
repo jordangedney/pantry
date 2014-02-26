b14ec17ec0aa62b6e375073398d4fbb167f92946
jordangedney <jjgedney@mtu.edu>
2014-02-26 16:15:40 -0500

    adding recipie template

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import views
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,6 +1,50 @@
 from app import app
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Todo: Adding, Deleting, Reading, Updating"
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+
+@app.route('/recipe')
+def recipe():
+    recipes = [ # fake recipes, until quereying works
+        { 
+            'name': 'Egg Sandwich', 
+            'ingredients': { 
+            				'eggs': '2',
+            				'bread': '2',  
+            				}, 
+           	'time': '10'
+        },
+        { 
+            'name': 'PBJ', 
+            'ingredients': { 
+            				'peanut butter': '2',
+            				'jelly': '2',
+            				'bread': '2',  
+            				}, 
+           	'time': '5'
+        }
+    ]
+    return render_template('recipe.html', recipes=recipes)
+
+
+
+
+
