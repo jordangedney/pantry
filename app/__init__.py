cb3a837f7e1ee9ac383b38860f7f22af13dfab20
Tyler Sanborn <tysanborn@gmail.com>
2014-03-01 20:43:17 -0500

    modifies __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,16 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+engine_url = sqlalchemy.engine.url.URL('mysql',
+                                        username='pantry',
+                                        password='pantrydb',
+                                        host='localhost',
+                                        port=3306,
+                                        database='Pantry',
+                                        query=None)
+
+engine = create_engine(engine_url)
+connection = engine.connect()
 
 from app import views, models
 
