611aea89f4446ba5b9124b89d6c98477826d3b09
jordangedney <jjgedney@mtu.edu>
2014-03-05 05:12:55 -0500

    adding print all users page

--- app/views.py
+++ app/views.py
@@ -20,28 +20,25 @@ def new_user():
     return render_template('new_user.html', form = form)
 
 
+@app.route('/get_users')
+def get_recipe():
+    users = models.User.query.all()
+    return render_template('print_users.html', users = users)
+
+
+
+
+
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="' + form.openid.data + 
-        	   '", remember_me=' + str(form.remember_me.data))
+               '", remember_me=' + str(form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', 
         title = 'Sign In',
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
-
-
-@app.route('/get_recipe')
-def get_recipe():
-    recipes = models.Recipe.query.all()
-    return jsonify(recipes)
-
-
-
-
-
-
-
