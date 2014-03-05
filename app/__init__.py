5af57f299f5f1ff3a82ab2ce5095088946c330e8
jordangedney <jjgedney@mtu.edu>
2014-03-05 04:16:29 -0500

    reverting back to sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,6 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config.from_object('config')
-engine_url = sqlalchemy.engine.url.URL('mysql',
-                                        username='pantry',
-                                        password='pantrydb',
-                                        host='localhost',
-                                        port=3306,
-                                        database='Pantry',
-                                        query=None)
-
 db = SQLAlchemy(app)
 
 from app import views, models
