9fb0784fa5a4223bc163b6c277c2db134b674eb5
jordangedney <jjgedney@mtu.edu>
2014-03-22 18:04:46 -0400

    added code to keep pycs from being generated

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-#!flask/bin/python
+# Keep .pyc files from being generated
+import sys
+sys.dont_write_bytecode = True
+
 from app import app
 app.run(debug = True, host='0.0.0.0', port=11235)
 #app.run(debug = True, host = '0.0.0.0', port = 80)
