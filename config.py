a421b3114e11f062bd03e4d6e07ca16d40b4e341
jordangedney <jjgedney@mtu.edu>
2014-03-24 19:22:54 -0400

    merging the folders

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'pantry.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'app/db_repository/migrate')
 
 
 CSRF_ENABLED = True
