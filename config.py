ce1908b9eb1e5a0315c0be5b20129e5e3355c4aa
Ben Frisbie <bbfrisbi@mtu.edu>
2014-03-26 16:47:14 -0400

    user hover dropdown

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'pantry.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'app/db_repository/migrate')
-
-
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
